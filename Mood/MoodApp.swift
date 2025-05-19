2c1b0b7688a7a8265de936c8ae91d1610ab84fd4
lois <b13605022@ntu.edu.tw>
2025-05-19 20:48:07 +0800

    Initial Commit

---
+++ Mood/MoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodApp.swift
+//  Mood
+//
+//  Created by Mac14 on 2025/5/19.
+//
+
+import SwiftUI
+
+@main
+struct MoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
